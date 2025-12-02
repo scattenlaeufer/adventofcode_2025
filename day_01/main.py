2729dbf5654405fb9376d9c88c6e4934205467d3
Björn Guth <scatty@bguth.de>
2025-12-02 13:34:07 +0100

    Solve day 01 part one

--- day_01/main.py
+++ day_01/main.py
@@ -1,5 +1,41 @@
+#!/usr/bin/env python3
+"""
+Day 01 on the Advent of Code
+"""
+
+from pathlib import Path
+
+
+def read_file(input_path: Path) -> list[str]:
+    with input_path.open() as input_file:
+        lines = [line.strip() for line in input_file]
+
+    return lines
+
+
 def main():
-    print("Hello from day-01!")
+    input_list = read_file(Path("input"))
+
+    lock = list(range(100))
+
+    step = 50
+    counter = 0
+    for order in input_list:
+        direction = order[0]
+        number = int(order[1:])
+
+        match direction:
+            case "L":
+                steps = -1 * number
+            case "R":
+                steps = number
+
+        step = lock[(step + steps) % len(lock)]
+
+        if step == 0:
+            counter += 1
+
+    print(f"The searched number is {counter}")
 
 
 if __name__ == "__main__":
