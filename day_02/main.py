3ed96cbf7984b0b54bd0655117a71b3e946cca9a
Björn Guth <scatty@bguth.de>
2025-12-03 19:20:16 +0100

    Solve day 02 part two

--- day_02/main.py
+++ day_02/main.py
@@ -15,23 +15,33 @@ def read_input_file(input_path: Path) -> list[list[int]]:
 
     return [[int(i) for i in r.split("-")] for r in input_str.split(",")]
 
+
 def day_02(input_path: Path) -> int:
     input_list = read_input_file(input_path)
 
     id_sum = 0
 
     for start, end in input_list:
-        for i in range(start, end+1):
-            i_str = str(i)
-            if len(i_str) % 2 == 0:
-                if i_str[:int(len(i_str)/2)] == i_str[int(len(i_str)/2):]:
-                    id_sum += i
+        for id_int in range(start, end + 1):
+            found = False
+            id_str = str(id_int)
+            for i in range(1, len(id_str)):
+                if found:
+                    continue
+                id_split = [id_str[j : j + i] for j in range(0, len(id_str), i)]
+                if all(id_split[0] == s for s in id_split):
+                    id_sum += id_int
+                    found = True
 
     return id_sum
 
 
 def main():
-    print(f"The sum of invalid IDs from test input is {day_02(Path('test_input'))}")
+    target = 4174379265
+    result = day_02(Path("test_input"))
+    print(
+        f"The sum of invalid IDs from test input is {result}, should be {target}, diff is {result - target}"
+    )
     print(f"The sum of invalid IDs from real input is {day_02(Path('input'))}")
 
 
