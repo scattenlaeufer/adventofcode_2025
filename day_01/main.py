1816b62a6340cdb57c0dbf8cd69ef8200241640f
Björn Guth <scatty@bguth.de>
2025-12-03 16:50:12 +0100

    [WIP] Work on part 2 of day 01

--- day_01/main.py
+++ day_01/main.py
@@ -13,29 +13,62 @@ def read_file(input_path: Path) -> list[str]:
     return lines
 
 
-def main():
-    input_list = read_file(Path("input"))
+def directive(order: str, step: int, lock: int = 100) -> (int, int):
+    direction = order[0]
+    number = int(order[1:])
 
-    lock = list(range(100))
+    counter_diff = 0
+
+    print(f"old_{step=}")
+
+    match direction:
+        case "L":
+            steps = -1 * number
+        case "R":
+            steps = number
+
+    new_step = step + steps
+    if steps < 0:
+        new_step = step+steps
+        if new_step < 0 and step != 0:
+            counter_diff += 1
+    counter_diff += int(abs(new_step) / lock)
+    # print(counter, step, steps, new_step, (step + steps) % lock)
+
+    print(f"{steps=}")
+
+    step = (step + steps) % lock
+
+    print(f"new_{step=}")
+
+    return step, counter_diff
+
+
+def day_01(input_path: Path) -> int:
+    input_list = read_file(input_path)
+
+    # lock = list(range(100))
+    lock = 100
 
     step = 50
     counter = 0
     for order in input_list:
-        direction = order[0]
-        number = int(order[1:])
+        print("================")
+        print(f"old_{counter=}")
+        print(f"{order=}")
 
-        match direction:
-            case "L":
-                steps = -1 * number
-            case "R":
-                steps = number
+        step, counter_diff = directive(order, step)
+        print(f"{counter_diff=}")
 
-        step = lock[(step + steps) % len(lock)]
+        counter += counter_diff
 
-        if step == 0:
-            counter += 1
+        print(f"new_{counter=}")
+    return counter
 
-    print(f"The searched number is {counter}")
+
+def main():
+    print(f"The searched test number is {day_01(Path('test_input'))}")
+    print(f"The searched number is {day_01(Path('input'))}")
 
 
 if __name__ == "__main__":
