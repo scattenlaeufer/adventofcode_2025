e5c7ee117c7a8f6d17d3cf2a892dbdd8219b003c
Björn Guth <scatty@bguth.de>
2025-12-04 10:33:31 +0100

    Create skeleton for day 04

--- day_04/main.py
+++ day_04/main.py
@@ -1,5 +1,37 @@
+#!/usr/bin/env python3
+"""
+Advent of Code 2025 day 04
+"""
+
+from pathlib import Path
+
+from rich import print
+
+
+def read_input_file(input_path: Path) -> list[str]:
+
+    with input_path.open() as input_file:
+        output_list = [l.strip() for l in input_file]
+
+    return output_list
+
+
+def day_04(input_path: Path) -> int:
+
+    input_list = read_input_file(input_path)
+
+    print(input_list)
+
+    return 0
+
+
 def main():
-    print("Hello from day-04!")
+    target = 13
+    result = day_04(Path("test_input"))
+    print(
+        f"The number of accessable roles in test input are {result}, target is {target}, diff {result-target}"
+    )
+    # print(f"The number of accessable roles in real input are {day_04(Path('input'))}")
 
 
 if __name__ == "__main__":
