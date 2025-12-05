f3968c2407a9b2198563536a281cba643e729e83
Björn Guth <scatty@bguth.de>
2025-12-05 18:22:59 +0100

    Create skeleton for day 05

--- day_05/main.py
+++ day_05/main.py
@@ -1,5 +1,48 @@
+#!/usr/bin/env python3
+"""
+Advent of Code 2025 day 05
+"""
+
+from pathlib import Path
+from rich import print
+
+
+def read_input_file(input_path: Path) -> list[list[str]]:
+
+    with input_path.open() as input_file:
+        input_list = [l.strip() for l in input_file]
+
+    processed_list = []
+    current_part = []
+
+    for line in input_list:
+        if not line:
+            processed_list.append(current_part)
+            current_part = []
+            continue
+        current_part.append(line)
+    processed_list.append(current_part)
+    return processed_list
+
+
+def day_05(input_path: Path) -> int:
+    input_list = read_input_file(input_path)
+    ids_fresh = 0
+
+    print(input_list)
+
+    return ids_fresh
+
+
 def main():
-    print("Hello from day-05!")
+    target = 3
+    result = day_05(Path("test_input"))
+    print(
+        f"the number of fresh ingredients IDs in test input are {result}, target is {target}, diff {result - target}"
+    )
+    print(
+        f"the number of fresh ingredients IDs in test input are {day_05(Path('input'))}"
+    )
 
 
 if __name__ == "__main__":
