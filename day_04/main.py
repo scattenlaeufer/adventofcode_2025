b44e8ca9533dfe113c9a1704a73d5613fa7ae65b
Björn Guth <scatty@bguth.de>
2025-12-05 17:50:32 +0100

    Solve day 04 part two

--- day_04/main.py
+++ day_04/main.py
@@ -16,30 +16,47 @@ def read_input_file(input_path: Path) -> list[str]:
     return output_list
 
 
-def day_04(input_path: Path) -> int:
+def find_roles(role_storage: list[list[str]]) -> int:
 
-    input_list = read_input_file(input_path)
-    max_roles = 3
     accessible_roles = 0
+    max_roles = 3
 
-    for i, row in enumerate(input_list):
+    new_role_storage = []
+
+    for i, row in enumerate(role_storage):
+        new_row = []
         for j, space in enumerate(row):
-            if space == "@":
+            if space == ".":
+                new_row.append(".")
+            else:
                 adjacent_spaces = []
-                for adjacent_row in input_list[
-                    max(0, i - 1) : min(len(input_list), i + 2)
+                for adjacent_row in role_storage[
+                    max(0, i - 1) : min(len(role_storage), i + 2)
                 ]:
                     adjacent_spaces.append(
                         adjacent_row[max(0, j - 1) : min(len(adjacent_row), j + 2)]
                     )
                 if sum(r.count("@") for r in adjacent_spaces) <= max_roles + 1:
                     accessible_roles += 1
+                    new_row.append(".")
+                else:
+                    new_row.append("@")
+        new_role_storage.append(new_row)
 
-    return accessible_roles
+    if accessible_roles:
+        return accessible_roles + find_roles(new_role_storage)
+    return 0
+
+
+def day_04(input_path: Path) -> int:
+
+    input_list = read_input_file(input_path)
+
+    return find_roles(input_list)
 
 
 def main():
-    target = 13
+    target = 43
     result = day_04(Path("test_input"))
     print(
         f"The number of accessable roles in test input are {result}, target is {target}, diff {result-target}"
