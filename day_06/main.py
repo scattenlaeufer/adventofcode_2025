7f95717357a0cfd6d9ac9fa1f702f151fb64f5e1
Björn Guth <scatty@bguth.de>
2025-12-06 19:08:30 +0100

    Solve day 06 part one

--- day_06/main.py
+++ day_06/main.py
@@ -7,10 +7,11 @@ from pathlib import Path
 from rich import print
 
 
-def read_input_file(input_path: Path) -> list[list[str | int]]:
+def read_input_file(input_path: Path) -> list[list[str]]:
     with input_path.open() as input_file:
         input_list = [
-            list(filter(None, (e.strip() for e in l.strip().split(" ")))) for l in input_file
+            list(filter(None, (e.strip() for e in l.strip().split(" "))))
+            for l in input_file
         ]
 
     return input_list
@@ -18,8 +19,17 @@ def read_input_file(input_path: Path) -> list[list[str | int]]:
 
 def day_06(input_path: Path) -> int:
     input_list = read_input_file(input_path)
-    print(input_list)
-    return 0
+    result_list: list[int] = [int(i) for i in input_list[0]]
+
+    for line in input_list[1:-1]:
+        for i, num in enumerate(line):
+            match input_list[-1][i]:
+                case "+":
+                    result_list[i] += int(num)
+                case "*":
+                    result_list[i] *= int(num)
+
+    return sum(result_list)
 
 
 def main():
@@ -28,7 +38,7 @@ def main():
     print(
         f"The sum of all results from test input is {result}, target is {target}, diff {result - target}"
     )
-    # print(f"The sum of all results from real input is {day_06(Path('input'))}")
+    print(f"The sum of all results from real input is {day_06(Path('input'))}")
 
 
 if __name__ == "__main__":
