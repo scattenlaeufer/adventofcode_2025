675e6749782e28493f9c72745735089055f2a5d4
Björn Guth <scatty@bguth.de>
2025-12-10 08:51:11 +0100

    Create skeleton for day 08

--- day_08/main.py
+++ day_08/main.py
@@ -1,5 +1,35 @@
+"""
+Advent of Code 2025
+"""
+
+from pathlib import Path
+from rich import print
+
+
+def read_input_file(input_path: Path) -> list[list[int]]:
+    with input_path.open() as input_file:
+        input_list = [list(int(i) for i in l.strip().split(",")) for l in input_file]
+
+    return input_list
+
+
+def day_08(input_path: Path, connections: int) -> int:
+    input_list = read_input_file(input_path)
+
+    print(input_list)
+
+    return 0
+
+
 def main():
-    print("Hello from day-08!")
+    connections = 10
+    result = day_08(Path("test_input"), connections)
+    target = 40
+    print(f"The product of curcuit lengths in test input is {result}, target is {target}, diff is {result - target}")
+
+    connections = 1000
+    result = day_08(Path("input"), connections)
+    print(f"The product of curcuit lengths in real input is {result}")
 
 
 if __name__ == "__main__":
